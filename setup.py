2dd136a431fb700f2727811242bec54cb0e84e90
w11wo <wilsonwong961@gmail.com>
2023-05-12 16:41:12 +0700

    Added Long Description

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from setuptools import find_packages, setup
+from pathlib import Path
+
+this_path = Path(__file__).parent
+
+readme_path = this_path / "README.md"
+
+long_description = readme_path.read_text(encoding="utf-8")
 
 setup(
     name="doeloe",
     version="0.0.1",
     description="A tool to convert old Indonesian language into EYD (Ejaan yang Disempurnakan)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="David Samuel",
     author_email="davidsamuel.7878@gmail.com",
     url="https://github.com/bookbot-hive/doeloe",
